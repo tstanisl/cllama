7c4dcbae5e04ce5512b387450c3215f86e06a426
Tomasz Stanislawski <toms@onspecta.com>
2026-08-06 19:47:24 +0200

    wip

--- cllama.c
+++ cllama.c
@@ -7,16 +7,9 @@ int main(int argc, char * argv[argc + 1]) {
     cgguf_h ctx = cgguf_open(argv[1]);
     ASSERT(ctx);
 
-/*
-    cgguf_keyval_s kv;
-    CGGUF_FOREACH_KEYVAL(ctx, kv) {
-        printf("key[%.*s]\n", (int)kv.key.len, kv.key.str);
-    }
-*/
-
-    cgguf_str_s key;
-    for (auto it = cgguf_iter(ctx); cgguf_peek(&it, &key, 0); cgguf_cont(&it)) {
-        printf("key[%.*s]\n", (int)key.len, key.str);
+    for (u64 i = 0; i < ctx->n_keyvals; ++i) {
+        cgguf_keyval_s kv = ctx->keyvals[i];
+        printf("key[%.*s]\n", (int)kv.key.size, kv.key.data);
     }
 
     cgguf_drop(ctx);
